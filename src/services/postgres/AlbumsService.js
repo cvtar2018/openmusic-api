58db447d8a915015d3a9edc2ed422637972cc118
Aldi <aldifayol@gmail.com>
2025-02-14 23:08:23 +0800

    add albums and songs db query services

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -1,20 +1,67 @@
-// const { Pool } = require('pg');
-// const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
-// class AlbumsService {
-//   constructor() {
-//     this._pool = new Pool();
-//   }
+class AlbumsService {
+  constructor() {
+    this._pool = new Pool();
+  }
 
-//   async addAlbum({ name, year }) {
-//     const id = nanoid(16);
-//     const createdAt = new Date().toISOString();
-//     const updatedAt = createdAt;
+  async addAlbum({ name, year }) {
+    const id = nanoid(16);
+    const albumId = `album-${id}`;
 
-//     const query = {
-//       text: 'INSERT INTO ',
-//     };
-//   }
-// }
+    const query = {
+      text: 'INSERT INTO albums VALUES($1, $2, $3) RETURNING id',
+      values: [albumId, name, year],
+    };
 
-// module.exports = AlbumsService;
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError();
+    }
+  }
+
+  async getAlbumById(id) {
+    const query = {
+      text: 'SELECT * FROM albums WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._poll.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Album id was not found');
+    }
+  }
+
+  async editAlbumById(id, { name, year }) {
+    const query = {
+      text: 'UPDATE notes SET name = $1, year = $2 WHERE id = $3 RETURNING id',
+      values: [name, year, id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Failed to Edit Album. Id was not found');
+    }
+  }
+
+  async deleteAlbumById(id) {
+    const query = {
+      text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Failed to delete. Album ID was not found');
+    }
+  }
+}
+
+module.exports = AlbumsService;
