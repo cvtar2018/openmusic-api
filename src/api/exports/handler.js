2fe1b43f22360e8e7f0aa39230bfedf698e77ecb
Aldi <aldifayol@gmail.com>
2025-03-13 06:51:53 +0800

    add export playlists feature implementation

---
+++ src/api/exports/handler.js
@@ -0,0 +1,31 @@
+const autoBind = require('auto-bind');
+// const ClientError = require('../../exceptions/ClientError');
+
+class ExportsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postExportPlaylistHandler(request, h) {
+    this._validator.validateExportPlaylistPayload(request.payload);
+
+    const message = {
+      userId: request.auth.credentials.id,
+      targetEmail: request.payload.targetEmail,
+    };
+
+    await this._service.sendMessage('export:playlists', JSON.stringify(message));
+
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda dalam antrean',
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ExportsHandler;
